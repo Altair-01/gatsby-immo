dc93853e97e2cf3919c45209549e19647b5b16ce
Altair-01 <omzosonko@gmail.com>
2022-08-18 12:36:35 +0000

    gatsby tuto part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,16 @@ module.exports = {
     title: `My first Gatsby Site`
   },
   plugins: [
+    
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp"
-  ]
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
+  ],
 };
